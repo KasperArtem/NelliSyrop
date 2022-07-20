9f0fa984153e110b68c69fc90b2bde9079db6349
KasperArtem <32073051+KasperArtem@users.noreply.github.com>
2022-07-20 20:24:45 +0300

    Swiper adaptation, color fix for mobile

--- js/Swiper.js
+++ js/Swiper.js
@@ -6,4 +6,18 @@ var swiper = new Swiper('.swiper', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-});
+    breakpoints: {
+        992: {
+            slidesPerView: 6,
+            spaceBetween: 0,
+        },
+        740:{
+            slidesPerView: 3,
+        },
+        500:{
+            slidesPerView: 2,
+        },
+        100:{
+            slidesPerView: 1,
+        }
+}});
